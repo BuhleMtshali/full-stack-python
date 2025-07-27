f3e2e6e40f10bddaa956ce0fa9614d10c2e68790
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-27 11:34:08 +0200

    updated the age calculator

---
+++ day-02/age_calculator.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+print('--------- Welcome to the Mini Age Calculator🍄 --------')
+name = input('Enter your name: ')
+year = int(input(f'Great, What year were you born in {name.title()}: '))
+month = int(input('What month were you born in ? (1-12): '))
+
+#getting today's date
+today = datetime.now()
+
+#creating the birthday object
+birth_date = datetime(year, month, 1)
+
+#calculating the difference in months
+years_diff = today.year - birth_date.year
+months_diff = today.month - birth_date.month
+
+#adjusting if the birthday hasn't cum yet
+if months_diff < 0:
+    years_diff -= 1
+    months_diff += 1
+
+print(f"Okay {name.title()}, You are {years_diff} years and {months_diff} months old🎁")
