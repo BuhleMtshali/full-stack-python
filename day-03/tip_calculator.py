3c7c45f67d8fd5e6914137abf2025d1523f460f0
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-28 14:49:11 +0200

    updated the tip amount

--- day-03/tip_calculator.py
+++ day-03/tip_calculator.py
@@ -1,9 +1,9 @@
 #greeting
 print("----- Welcome to My Mini Tip Calculator💰 -----")
 
-order_total = int(input('Enter your order total: '))
+order_total = float(input('Enter your order total: '))
 
-tip_amount = int(input('Enter the tip percentage you wanna leave(0% - 100%): '))
+tip_amount = float(input('Enter the tip percentage you wanna leave(0 - 100): '))
 
 converted_tip_amount = (tip_amount / 100) * order_total
 
@@ -11,7 +11,19 @@ overall_total = order_total + converted_tip_amount
 
 number_of_people = int(input('How many people are the dividing the order total with: '))
 
-amount_per_person = overall_total / number_of_people
-
-print(f"Each person will pay: {amount_per_person:.2f}")
+if number_of_people == 0:
+    print("Okay friend the whole bill is on you!!")
+    print("======== Order Receipt🧾 =========")
+    print(f"Order Total: R{order_total:.2f}")
+    print(f"Tip Amount: {tip_amount}%")
+    print(f"Overall Total: R{overall_total:.2f}")
+else:
+    amount_per_person = overall_total / number_of_people
+    print("======== Order Receipt🧾 =========")
+    print(f"Order Total: R{order_total:.2f}")
+    print(f"Tip Amount: {tip_amount}%")
+    print(f"Overall Total: R{overall_total:.2f}")
+    print(f"Number of People Sharing the Bill: {number_of_people}")
+    print(f"Amount Per Person: R{amount_per_person:.2f}")
+    print("========= Thank You For Trying My Tip Calculator =======")
 
