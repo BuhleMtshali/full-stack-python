9ce7f67e5cf25b33cd28d5b07b984785295317af
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-28 14:26:46 +0200

    updated the tip amount

--- day-03/tip_calculator.py
+++ day-03/tip_calculator.py
@@ -2,3 +2,8 @@
 print("----- Welcome to My Mini Tip Calculator💰 -----")
 
 order_total = int(input('Enter your order total: '))
+
+tip_amount = int(input('Enter the tip percentage you wanna leave(0% - 100%): '))
+
+number_of_people = int(input('How many people are the dividing the order total with: '))
+
