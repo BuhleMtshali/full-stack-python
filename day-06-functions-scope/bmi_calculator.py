42e674cb6e124dbff5e9bae54de6aa5b931f52ee
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-02 11:01:49 +0200

    updated the measurements in the calculator

--- day-06-functions-scope/bmi_calculator.py
+++ day-06-functions-scope/bmi_calculator.py
@@ -3,11 +3,11 @@ def get_BMI():
     print("===== Welcome to My mini BMI Calculator🧮 =====")
     user_name = input("Enter your name: ")
     user_weight = float(input("Enter your weight in(kg): "))
-    user_height = float(input("Enter your height in(m): "))
+    user_height_cm = float(input("Enter your height in(cm): "))
+    user_height = user_height_cm / 100
+    overall_BMI = user_weight / (user_height ** 2)
 
-    overall_BMI = user_weight * (user_height ** 2)
-
-    print(f"Okay {user_name}, your weight is {user_weight}kg and your height is {user_height}m, so with that information, we have calculated your overall BMI to be {overall_BMI}kg/m^2")
+    print(f"Okay {user_name}, your weight is {user_weight}kg and your height is {user_height}m, so with that information, we have calculated your overall BMI to be {overall_BMI:.2f}kg/m^2")
 
 
 get_BMI()
