8c6280d92ed076ee5628947f2060e76d429611d4
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-07 00:34:26 +0200

    updated the phone book

---
+++ day-08-tuples/phone_book.py
@@ -0,0 +1,17 @@
+#main list 
+phone_book = []
+
+
+#greet message
+greeting = print(" ===== 📵Welcome To Your Mini PhoneBook☎️ =====")
+
+print(greeting)
+
+
+while True:
+    print("Here i am startig a loop")
+
+
+    add_again = input("\nWanna add another contact? (yes/no): " ).strip().lower()
+    if add_again != "yes":
+        print("\nThank you for trying Mini Phonebook app!")
