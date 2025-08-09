97f50b7b9255f46bc98aa98422bb87aacfeb05d6
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-09 15:48:18 +0200

    created the first function

--- day-08-tuples/phone_book.py
+++ day-08-tuples/phone_book.py
@@ -7,9 +7,18 @@ greeting = print(" ===== 📵Welcome To Your Mini PhoneBook☎️ =====")
 
 print(greeting)
 
+#creatng a function for viewing the phone phonebook
+def view_contact_list():
+    """view the contact list"""
+    if len(phone_book) > 0:
+        for phone in phone_book:
+            print(f"Contact: {phone}")
+    else:
+        print("🚫Phone Book is currently empty")
+
 
 while True:
-    print("Here i am startig a loop")
+    print("1. View Contact")
 
 
     add_again = input("\nWanna add another contact? (yes/no): " ).strip().lower()
