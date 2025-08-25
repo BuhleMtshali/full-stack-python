b70360fc4b15a8cf5f585869632abc02dbcd6296
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-25 11:38:35 +0200

    updated the safe calculator

--- day-09-try-except/safe_calculator.py
+++ day-09-try-except/safe_calculator.py
@@ -1,48 +1,41 @@
 print("------ Welcome to my Mini Safe Calculator📝 -------")
 
-#STARTING THE WHILE LOOP
 while True:
     try:
         first_number = float(input("Enter your first digit: "))
     except ValueError:
-        print("Oops, invalid input, please try again")
+        print("🚫 Oops, invalid input, please try again")
         continue
-    
-    try:
-        operator = input("Choose your operator (+, -, *, /): ")
-    except ValueError:
-        print("Invalid operator, please try again!")
+
+    operator = input("Choose your operator (+, -, *, /): ")
+    if operator not in ["+", "-", "*", "/"]:
+        print("🚫 Invalid operator, please try again!")
         continue
 
     try:
         second_number = float(input("Enter your second digit: "))
     except ValueError:
-        print("Invalid number, please try again")
+        print("🚫 Invalid number, please try again")
         continue
-    
+
     result = 0
 
-    #IF STATEMENT
     if operator == "+":
         result = first_number + second_number
-
     elif operator == "-":
         result = first_number - second_number
-    
     elif operator == "*":
         result = first_number * second_number
-
     elif operator == "/":
-        if first_number == 0:
-            print("Cannot divide by 0")
+        if second_number == 0:
+            print("🚫 Cannot divide by 0")
+            continue
         else:
             result = first_number / second_number
 
+    print(f"✅ Result: {result}")
 
-
-    print(f"Result: {result}")
-
-    calculate_again = input("\nWanna make another calculation? (yes/no): ")
+    calculate_again = input("\nWanna make another calculation? (yes/no): ").strip().lower()
     if calculate_again != "yes":
-        print("\nThank you trying my mini calculator!!!")
-        break;
+        print("\n🙏 Thank you for trying my mini calculator!!!")
+        break
