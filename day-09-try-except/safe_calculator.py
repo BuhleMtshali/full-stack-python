de138e4b64d5ba36a2068e2d52652a756ce73ad9
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-25 11:04:33 +0200

    updated the safe calculator and started the while loop

--- day-09-try-except/safe_calculator.py
+++ day-09-try-except/safe_calculator.py
@@ -1,3 +1,10 @@
 print("------ Welcome to my Mini Safe Calculator📝 -------")
 
+#STARTING THE WHILE LOOP
+while True:
+    print("start of the loop")
 
+    calculate_again = input("\nWanna make another calculation? (yes/no): ")
+    if calculate_again != "yes":
+        print("\nThank you trying my mini calculator!!!")
+        break;
