3575b16e27ee114e10fe6b2a8c1de100e82474d1
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-25 11:21:17 +0200

    added the second try statement

--- day-09-try-except/safe_calculator.py
+++ day-09-try-except/safe_calculator.py
@@ -7,8 +7,13 @@ while True:
     except ValueError:
         print("Oops, invalid input, please try again")
         continue
+    
+    try:
+        operator = input("Choose your operator (+, -, *, /): ")
+    except ValueError:
+        print("Invalid operator, please try again!")
 
-    operator = input("Choose your operator (+, -, *, /): ")
+    
     second_number = float(input("Enter your second digit: "))
 
     calculate_again = input("\nWanna make another calculation? (yes/no): ")
