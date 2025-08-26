5783c28d9a5032e733a254a39186a49ed3f5de6f
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-26 11:34:00 +0200

    updated the main file

--- day-10-review-build/rock_paper_scissors.py
+++ day-10-review-build/rock_paper_scissors.py
@@ -11,7 +11,7 @@ print('''Rock, Paper, Scissor
 
 #variables to keep track of the number of wins, loses and ties
 wins = 0
-loses = 0
+losses = 0
 ties = 0
 
 
@@ -19,7 +19,7 @@ ties = 0
 while True:
     #INNER while loop thats keeps asking the player to enter R,P,S or Q
     while True:
-        print('{} Wins, {} Losses, {} Ties'.format(wins, loses, ties))
+        print('{} Wins, {} Losses, {} Ties'.format(wins, losses, ties))
 
         print('Enter your move: (R)ock (P)aper (S)cissors or (Q)uit')
         playerMove = input('> ').upper()
