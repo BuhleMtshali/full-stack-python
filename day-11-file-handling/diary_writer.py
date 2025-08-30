96bf23bf86c2bd9784f68d51c9fdf3ee3abbfc1a
Buhle <mtshalizanobuhle7@gmail.com>
2025-08-30 11:05:23 +0200

    updated the diary writer

--- day-11-file-handling/diary_writer.py
+++ day-11-file-handling/diary_writer.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 path = Path('/Users/buhlemtshali/Desktop/Python-Full-Stack/full-stack-python/day-11-file-handling/pi_digits.txt')
 contents = path.read_text()
+print(contents)
 lines = contents.splitlines()
 for line in lines:
     print(line)
