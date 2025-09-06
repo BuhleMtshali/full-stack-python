ef6bffa11ad350208b9d6963179db4baa4cc45b0
Buhle <mtshalizanobuhle7@gmail.com>
2025-09-06 18:23:53 +0200

    updated the diary writer

--- day-11-file-handling/diary_writer.py
+++ day-11-file-handling/diary_writer.py
@@ -5,3 +5,7 @@ from datetime import datetime
 def write_entry():
     """Allows the user to enter a new diary entry"""
     entry_text = input("Enter your diary entry (type 'DONE' on a new line to finish): \n")
+    entry_lines = []
+    while entry_text.strip().upper() != 'DONE':
+        entry_lines.append(entry_text)
+        entry_text = input()
