8798d13af0baf1806495d17caa85fdada1d9646a
Buhle <mtshalizanobuhle7@gmail.com>
2025-09-06 18:20:25 +0200

    created the diary entry functrion

---
+++ day-11-file-handling/diary_writer.py
@@ -0,0 +1,7 @@
+# importing date
+from datetime import datetime
+
+# function for diary entry
+def write_entry():
+    """Allows the user to enter a new diary entry"""
+    entry_text = input("Enter your diary entry (type 'DONE' on a new line to finish): \n")
