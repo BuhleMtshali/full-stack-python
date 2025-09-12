d1dc20e50e67cadb0bffe2245724077ec92d049e
Buhle <mtshalizanobuhle7@gmail.com>
2025-09-12 10:35:26 +0200

    created the while loop

--- day-12-lists-sets/mini_shopping_cart.py
+++ day-12-lists-sets/mini_shopping_cart.py
@@ -1,4 +1,13 @@
 #MAIN CART
 shopping_cart = {"bread", "cheese"}
 
-print("--------- 📝 Welcome to My Mini Shopping Cart 🛒 --------")
+print("\n--------- 📝 Welcome to My Mini Shopping Cart 🛒 --------")
+
+# START THE WHILE LOOP
+
+while True:
+    try:
+        print("this is the try statement")
+    except ValueError:
+        print("Oppps wrong option")
+        continue
