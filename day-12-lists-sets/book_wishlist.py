f68e6e24f3d9364a1dfe2134248d66da5d4de4ba
Buhle <mtshalizanobuhle7@gmail.com>
2025-09-12 10:51:59 +0200

    updated the booklist

--- day-12-lists-sets/book_wishlist.py
+++ day-12-lists-sets/book_wishlist.py
@@ -4,5 +4,13 @@ book_list = []
 # HERE I'M GONNA CREATE FUNCTIONS FOR EACH OPTION
 def greeting_message():
     print("\n" + "=" * 45)
-    print("===== 🍄 Welcome To My Mini Grocery List 🛒 =====")
+    print("===== 🍄 Welcome To My Mini Bookwishlist 📚 =====")
     print("=" * 45 + "\n")
+
+
+def show_menu():
+    print("1. View Books 📖")
+    print("2. Add Book 📒")
+    print("3. Remove Book 🗑️")
+    print("4. Mark as Read 🥸")
+    print("5. Exit 🚫")
