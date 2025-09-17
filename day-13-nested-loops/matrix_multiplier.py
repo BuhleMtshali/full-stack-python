a03ca38f5a90fc39bcaf75705b2cedf711142ec6
Buhle <mtshalizanobuhle7@gmail.com>
2025-09-17 22:47:05 +0200

    created the nested arrays

--- day-13-nested-loops/matrix_multiplier.py
+++ day-13-nested-loops/matrix_multiplier.py
@@ -1,5 +1,15 @@
 # NESTED LOOPS
-for i in range(1, 11):
-    print(f"🍄This is from the outer loop: {i}")
-    #for j in range(1, 11):
-        #print(f"🍀This is from the inner loop: {j}")
+for i in range(1, 5):
+    for j in range(1, 5):
+        print(f"{i} x {j} = {i * j}")
+
+
+# NESTED 2D LISTS
+nested_list = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for first_row in nested_list:
+    print(first_row)
